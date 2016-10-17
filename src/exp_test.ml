23994ad8d7f871ddb467dc046edb482042ddfa86
dxj452 <dxj452@cs.bham.ac.uk>
2016-10-17 12:13:48 +0100

    Update test script
    The test results are now pre-fixed by the name of the file that is being testing.
    I also added three test cases that always fail.

--- src/exp_test.ml
+++ src/exp_test.ml
@@ -1,24 +1,22 @@
 open Exp_lex
 open Lexing
 open Printf
-open Tree_parse
 
-let print_position lexbuf =
+let get_position lexbuf =
   let pos = lexbuf.lex_curr_p in
-  eprintf "Pos %d:%d:%d\n" pos.pos_lnum pos.pos_bol pos.pos_cnum
+  sprintf "Pos %d:%d:%d\n" pos.pos_lnum pos.pos_bol pos.pos_cnum
 
-let parse_with_error lexbuf =
-  try Exp_par.top Exp_lex.read lexbuf with
-  | SyntaxError msg -> prerr_string (msg ^ ": ");
-                       print_position lexbuf;
-                       exit (-1)
-  | Exp_par.Error ->   prerr_string "Parse error: ";
-                       print_position lexbuf;
-                       exit (-1)
+let test_parse_with_error lexbuf =
+  try
+    let _ = Exp_par.top Exp_lex.read lexbuf in "pass\n"
+  with
+    | SyntaxError msg -> msg ^ ": " ^ get_position lexbuf
+    | Exp_par.Error ->   "Parse error: " ^ get_position lexbuf
+    | _ -> "Unhandled error\n"
 
 let parse_test str =
   let lex = Lexing.from_string str in
-  let _ = parse_with_error lex in print_string "pass\n"
+  test_parse_with_error lex
 
 let read_file filename =
   let lines = ref [] in
@@ -31,22 +29,35 @@ let read_file filename =
     close_in chan;
     String.concat "" (List.rev !lines);;
 
+let test_files = [
 (*Small test cases *)
-parse_test (read_file "tests/small_tests/test_application.jk");
-parse_test (read_file "tests/small_tests/test_assignment.jk");
-parse_test (read_file "tests/small_tests/test_const.jk");
-parse_test (read_file "tests/small_tests/test_dereference.jk");
-parse_test (read_file "tests/small_tests/test_identifier.jk");
-parse_test (read_file "tests/small_tests/test_if_else.jk");
-parse_test (read_file "tests/small_tests/test_let.jk");
-parse_test (read_file "tests/small_tests/test_new.jk");
-parse_test (read_file "tests/small_tests/test_operator.jk");
-parse_test (read_file "tests/small_tests/test_negate.jk");
-parse_test (read_file "tests/small_tests/test_print_int.jk");
-parse_test (read_file "tests/small_tests/test_read_int.jk");
-parse_test (read_file "tests/small_tests/test_sequence.jk");
-parse_test (read_file "tests/small_tests/test_while.jk");
+"tests/small_tests/test_application.jk";
+"tests/small_tests/test_assignment.jk";
+"tests/small_tests/test_const.jk";
+"tests/small_tests/test_dereference.jk";
+"tests/small_tests/test_fun_args.jk";
+"tests/small_tests/test_identifier.jk";
+"tests/small_tests/test_if_else.jk";
+"tests/small_tests/test_let.jk";
+"tests/small_tests/test_multiple_functions.jk";
+"tests/small_tests/test_negate.jk";
+"tests/small_tests/test_new.jk";
+"tests/small_tests/test_operator.jk";
+"tests/small_tests/test_print_int.jk";
+"tests/small_tests/test_read_int.jk";
+"tests/small_tests/test_sequence.jk";
+"tests/small_tests/test_while.jk";
+(* Large test cases *)
+"tests/large_tests/iterative_bisection.jk";
+"tests/large_tests/recursive_bisection.jk";
+(* These tests should fail *)
+"tests/small_tests/test_fail_if.jk";
+"tests/small_tests/test_fail_operation.jk";
+"tests/small_tests/test_fail_sequence.jk";
+];;
 
-(*Large test cases *)
-parse_test (read_file "tests/large_tests/iterative_bisection.jk");
-parse_test (read_file "tests/large_tests/recursive_bisection.jk");
+let rec batch_test files = match files with
+  | []    -> ""
+  | x::xs -> (x) ^ ": " ^ parse_test (read_file x) ^ batch_test xs;;
+
+print_string (batch_test test_files);
